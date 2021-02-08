bf9f4ff92d4eb78a19833afa4ea987219b9f201d
Semen Sukhorada <s.suhorada@dunice.net>
2021-02-08 06:40:38 -0800

    верстка 3 направлений

---
+++ testMove.js
@@ -0,0 +1,13 @@
+function test (x,y) 
+{
+    this.width = 20;
+    this.height = 20;
+    this.speedX = 0;
+    this.speedY = 0;
+    this.x = x;
+    this.y = y;
+    this.update = function () 
+    {
+        
+    }   
+}
